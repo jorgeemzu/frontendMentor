270164c5146db66ccadec84eef77a77ccdb62515
jorgeemzu <63023978+jorgeemzu@users.noreply.github.com>
2020-05-04 22:43:19 -0400

    Add files via upload
    Mobile first CSS Grid and Javascript

---
+++ social-media-dashboard-with-theme-switcher-master/darkMode.js
@@ -0,0 +1,18 @@
+
+function darkMode(){
+    let checkbox = document.getElementById('checkbox');
+    // let Dark = document.querySelector('body')
+    if(checkbox.checked){
+       document.body.className = 'darkMode';
+}else
+
+    document.body.classList.remove('darkMode');
+
+}
+
+
+    
+
+ 
+ 
+
